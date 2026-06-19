981c3e58ae63849d73ec824a3fd7da599c4d51b0
jade <jadesada.jung@gmail.com>
2026-06-20 03:20:25 +0700

    add prisma module , auth

---
+++ backend/src/auth/auth.service.ts
@@ -0,0 +1,40 @@
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma.service';
+import { JwtService } from '@nestjs/jwt'
+import { LoginDto } from './dto/login.dto';
+import bcrypt from 'bcrypt'
+
+
+@Injectable()
+export class AuthService {
+  constructor(
+    private prisma: PrismaService,
+    private jwtService: JwtService
+  ) { }
+
+  async login(loginDto: LoginDto) {
+    const user = await this.prisma.admin.findUnique({
+      where: { username: loginDto.username }
+    })
+
+    if (!user) { throw new UnauthorizedException() }
+
+    const isValid = await bcrypt.compare(loginDto.password, user.password)
+
+    if (!isValid) { throw new UnauthorizedException() }
+
+    const payload = {
+      sub: user.id
+    }
+
+    const accessToken = await this.jwtService.signAsync(payload)
+
+    return { accessToken, id: user.id, username: user.username };
+  }
+
+  logout() {
+    return {
+      message: "Logout Successfully"
+    }
+  }
+}
