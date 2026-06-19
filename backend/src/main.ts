e45a63410fea3f38531c96b21479730a7f37dbdf
jade <jadesada.jung@gmail.com>
2026-06-20 03:08:32 +0700

    add cors , cookie & prisma service

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,8 +1,35 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ConfigService } from '@nestjs/config'
+import cookieParser from 'cookie-parser'
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+
+  const configService = app.get(ConfigService)
+
+  app.enableCors({
+    origin: configService.get("FRONTEND_URL"),
+    Credential: true
+  })
+
+  app.use(cookieParser())
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      transform: true
+    })
+  )
+
+  app.setGlobalPrefix('/')
+
+  const port = configService.get<number>('PORT') || 6060
+
+  await app.listen(port);
+
+  console.log(`Service is running on ${port}`)
 }
 bootstrap();
